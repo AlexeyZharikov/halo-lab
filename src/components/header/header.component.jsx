2e1fee6754e02c1b5d41eb31c2ebd9ae97c466fd
AlexeyZharikov <mefis2.0@gmail.com>
2022-09-22 16:42:16 +0300

    second

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainMenu from './main/mainMenu.component';
 import Burger from './burger/burger.component';
 import images from '../../assets/images';
@@ -7,12 +7,26 @@ import './header.scss';
 import './header.mobile.scss';
 
 const Header = () => {
+  
+  const [hover, setHover] = useState(false)
+
+  const hoverStyle = { "transform": "scale(1.2)", "transition" : "1s",};
+  const normalStyle = {"transition" : "1s",}
+
+  const onMouseEnter = () => {
+    setHover(true)
+  }
+
+  const onMouseLeave = () => {
+    setHover(false)
+  }
+
   return (
     <header className="header">
       <div className="container">
         <div className="header-inner">
           <div className="header-logo">
-            <img src={images.logo} alt="logo"/>
+            <img src={images.logo} alt="logo" style={hover ? hoverStyle : normalStyle} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}/>
           </div>
           <MainMenu/>
           <Burger/>
